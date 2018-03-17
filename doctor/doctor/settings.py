2f11bc308a56839a01616732d534c9c40df764e0
studychao <31121654+studychao@users.noreply.github.com>
2018-03-18 01:47:04 +0800

    实现了从数据库里提取文献和新闻到页面

--- doctor/doctor/settings.py
+++ doctor/doctor/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'index',
     'account',
+  'infocenter'
 ]
 
 MIDDLEWARE = [
---
+++ doctor/infocenter/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InfocenterConfig(AppConfig):
+    name = 'infocenter'
