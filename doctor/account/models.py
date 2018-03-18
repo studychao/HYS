976cb08ea76e95c9b5c812190b34afb5f4ef8159
Zouuuuuuu <37393689+Zouuuuuuu@users.noreply.github.com>
2018-03-18 16:02:24 +0800

    account

--- doctor/account/models.py
+++ doctor/account/models.py
@@ -7,8 +7,8 @@ from django.conf import settings
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 
     def __unicode__(self):
-        return self.username
+        return self.email
--- doctor/account/views.py
+++ doctor/account/views.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django import forms
+from models import User
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect,HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 # Create your views here.
 
+class UserForm(forms.Form):
+    email = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
+
 def sign_in(request):
-    return render(request,'sign-in.html')
+    return render_to_response('sign-in.html')
 
 def sign_up(request):
-    return render(request,'sign-up.html')
+	Method = request.method
+	if Method == 'POST':
+		uf = UserForm(request.POST)
+		if uf.is_valid():
+			email = uf.cleaned_data['email']
+			password = uf.cleaned_data['password']
+			try:
+				registJudge = User.objects.filter(email=email).get().email
+				return render_to_response('sign-in.html',{'registJudge':registJudge})
+			except:
+				registAdd = User.objects.create(email=email,password=password)
+			#registAdd = User.objects.get_or_create(username=username,password=password)[1]
+			#if registAdd == False:
+				return render_to_response('sign-in.html',{'registAdd':registAdd,'email':email})
+	else:
+		uf = UserForm()
+	return render_to_response('sign-up.html',{'uf':uf,'Method':Method})#,context_instance=RequestContext(req)
+   # return render(request,'sign-up.html')
