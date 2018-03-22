bb9299054db3a17f99c2b69c5672422cfa4fa01e
Zouuuuuuu <37393689+Zouuuuuuu@users.noreply.github.com>
2018-03-23 00:03:36 +0800

    分页功能

--- doctor/doctor/settings.py
+++ doctor/doctor/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'dj_pagination',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'dj_pagination.middleware.PaginationMiddleware',
 ]
 
 ROOT_URLCONF = 'doctor.urls'
